eb5dd6569b2890fbc331bc4fe790622ecb0a7563
Marcelo Camargo <marcelocamargo@linuxmail.org>
2015-11-07 00:08:50 -0200

    Action composition

--- test.php
+++ test.php
@@ -9,3 +9,15 @@ require_once 'src/DataType/Int.php';
 use \Rawr\DataType\Action;
 use \Rawr\DataType\Bool;
 use \Rawr\DataType\Int;
+
+$add10 = new Action(function($x, Int $y) {
+  return $x + $y->value();
+}, 10);
+
+$multiply = new Action(function($x, $y) {
+  return $x + $y * 3;
+}, 5);
+
+$add_and_double = $add10->·($multiply);
+
+var_dump($add_and_double(20)); // => Int(70)
